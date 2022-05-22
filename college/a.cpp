8f056313ffe26c34654340243a0d53d09fb579f2
Nishant Singhal <nishantsinghal900@gmail.com>
2022-05-22 12:06:39 +0530

    college

---
+++ college/a.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[4]={1,2,3,4};
+    for (int &i : arr)
+    {
+        cout<<i;
+    }
+    
+    return 0;
+}
