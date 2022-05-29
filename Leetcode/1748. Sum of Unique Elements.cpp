4a0223b33dd1f9db30897d018d81fdf24706af09
Nishant Singhal <nishantsinghal900@gmail.com>
2022-05-29 21:51:51 +0530

    leetcode

---
+++ Leetcode/1748. Sum of Unique Elements.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        unordered_map<int, int> m;
+    int res = 0;
+    for (auto n : nums) {
+        int cnt = ++m[n];
+        res += cnt == 1 ? n : cnt == 2 ? -n : 0;
+    }
+    return res;
+    }
+};
+
+
